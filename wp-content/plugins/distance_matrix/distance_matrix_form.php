256219be1e3914fd91f545f8591ba94e3c42a490
spencer10 <jeffspencer10@gmail.com>
2020-10-09 22:07:25 -0500

    Form Revised added

--- wp-content/plugins/distance_matrix/distance_matrix_form.php
+++ wp-content/plugins/distance_matrix/distance_matrix_form.php
@@ -14,33 +14,33 @@
     // wp_register_style( 'distance_matrix', plugins_url( 'includes/css/distance_matrix.css' ) );
     // wp_register_style( 'distance_matrix', plugins_url( 'includes/css/dForm.css' ) );
 
-    // global $wpdb;
-
-    // $allposts = $wpdb->get_results( $wpdb->prepare("SELECT ID, post_title FROM $wpdb->posts WHERE post_status = 'publish'") );
-    // foreach ($allposts as $singlepost) { 
-    //         echo '<p>' .$singlepost->post_title. '</p>';
-    // }
-
     echo "
-    <form id='distance-matrix' action='wp-content/plugins/distance_matrix/distance_matrix_api.php' method='get'>
+    <div id='distance-form'> 
 
-        <div id='distance-form'> 
-        <p id='form-title'>FREE SHIPPING QUOTE</p>
+        </br>
+    
+            <div id='title-box'>
+            <p id='form-title'>GET YOUR FREE SHIPPING QUOTE NOW</p>
+            </div>
+    
+        </br>
+  
+        <section id='form-input'>
 
-                <div class='vehicle-select' style='width: 95%;'>
-                    <select name='make'>
-                        <option value='0'>Vehicle</option>
-                        <option value='Motorcycle'>Motorcycle</option>
-                        <option value='ATV'>ATV</option>
-                        <option value='Snowmobile'>Snowmobile</option>
-                        <option value='JetSki'>Single Jet Ski</option>
+                <div class='vehicle-select'>
+                    <select>
+                    <option value='0'>Vehicle</option>
+                    <option value='1'>Motorcycle</option>
+                    <option value='2'>ATV</option>
+                    <option value='3'>Snowmobile</option>
+                    <option value='4'>Single Jet Ski</option>
                     </select>
                 </div>
 
-            </br>
+            <!-- </br> -->
 
-            <div class='year-select' style='width: 95%;'>
-                <select name='year'>
+            <div class='year-select'>
+                    <select>
                     <option value='0'>Year</option>
                     <option value='1'>2020</option>
                     <option value='2'>2019</option>
@@ -54,18 +54,18 @@
                     <option value='10'>2011</option>
                     <option value='11'>2010</option>
                     <option value='12'>2009</option>
-                </select>
-            </div>
+                    </select>
+                </div>
                 
             </br>
-            
-            <div class='make-select' style='width: 95%;'>
-                <select name='model'>
+    
+                <div class='make-select'>
+                    <select>
                     <option value='0'>Make</option>
-                    <option value='Audi'>Audi</option>
-                    <option value='BMW'>BMW</option>
+                    <option value='1'>Audi</option>
+                    <option value='2'>BMW</option>
                     <option value='3'>Citroen</option>
-                    <option value='Ford'>Ford</option>
+                    <option value='4'>Ford</option>
                     <option value='5'>Honda</option>
                     <option value='6'>Jaguar</option>
                     <option value='7'>Land Rover</option>
@@ -74,13 +74,13 @@
                     <option value='10'>Nissan</option>
                     <option value='11'>Toyota</option>
                     <option value='12'>Volvo</option>
-                </select>
-            </div>
+                    </select>
+                </div>
                 
-            </br>
+            <!-- </br> -->
 
-            <div class='model-select' style='width: 95%;'>
-                <select>
+                <div class='model-select'>
+                    <select>
                     <option value='0'>Model</option>
                     <option value='1'>Audi</option>
                     <option value='2'>BMW</option>
@@ -94,20 +94,36 @@
                     <option value='10'>Nissan</option>
                     <option value='11'>Toyota</option>
                     <option value='12'>Volvo</option>
-                </select>
-            </div>
+                    </select>
+                </div>
 
-            <p>Can't find your vehicle? Submit a custom quote.</p>
-            <!-- <label for='fname'>Origin:</label><br> -->
-            <input class='zip' type='text' id='fname' name='origin' value='Pick Up Zip Code'><br><br>
-            <!-- <label for='lname'>Destination:</label><br> -->
-            <input class='zip' type='text' id='lname' name='destination' value='Delivery Zip Code'><br><br>
-            <p>$7000 valuation coverage included.</p>
-            <p>Currently average transit time is 15 business days.</p>
+                <!-- <p>Can't find your vehicle? Submit a custom quote.</p> -->
+
+            <!-- // wp_register_style( 'my-plugin', plugins_url( 'ddd/css/plugin.css' ) ); -->
+
+            <!-- // wp_register_script( 'my-plugin', plugins_url( 'ddd/js/plugin.js' ) ); -->
+
+                
+                <form id='distance-matrix' action='wp-content/plugins/distance_matrix/distance_matrix_api.php' method='get'>
+                    <!-- <label for='fname'>Origin:</label><br> -->
+                    <input class='zip' type='text' id='fname' name='origin' value='Origin'><br><br>
+                    <!-- <label for='lname'>Destination:</label><br> -->
+                    <input class='zip' type='text' id='lname' name='destination' value='Destination'><br><br>
+            </section>
+                </br>
+                </br>
+                    <input id='distance-matrix-submit' type='submit' value= '    >  GET PRICING'>
+                </form>
+                </br>
+                <p>$7000 VALUATION COVERAGE INCLUDED.</p>
+                <p>CURRENTLY AVERAGE TRANSIT TIME IS 15 BUSINESS DAYS.</p>
             </br>
-            <input id='distance-matrix-submit' type='submit' value='FREE QUOTE'>
+            </br>
+            <p>DON'T SEE YOUR VEHICLE? WE ALSO TRANSPORT</p>
+            <p>BOATS, UTV'S & PWC's - <a style='color:green' href=''><i>CLICK HERE FOR A CUSTOM QUOTE ></i></a></p>
+            
         </div>
-    </form>
+  
     ";
 ?>
 <script src="wp-content/plugins/distance_matrix/includes/js/form.js"></script>
